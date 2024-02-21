199389c0a20fa2aa2810b69b3f87e1080ed68492
Nyko <52129064+Gotman08@users.noreply.github.com>
2024-02-21 13:05:26 +0100

    Update StatAnalyser.py

--- StatAnalyser.py
+++ StatAnalyser.py
@@ -4,19 +4,13 @@ diagrammes = True
 try:
     import matplotlib.pyplot as plt
 except ImportError:
-    print("L'opption des diagrammes indisponible")
+    print("L'option des diagrammes indisponible")
     diagrammes = False
 
 try:
     import numpy as np
 except ImportError:
-    print("L'opption des diagrammes indisponible")
-    diagrammes = False
-
-try:
-    import kandinsky as kd
-except ImportError:
-    print("L'opption des diagrammes indisponible")
+    print("L'option des diagrammes indisponible")
     diagrammes = False
 
 class Tab:
@@ -90,13 +84,9 @@ class Tab:
             data.append(hi)  
 
     def calculer(self):
-        
-        self.estimerPlagesManquantes()
-        
-        
         self.ci()
-        self.fi()
         self.ai()
+        self.fi()
         self.Fi()
         self.hi()
 
@@ -120,7 +110,7 @@ class Tab:
     def verif(self) -> bool:
         return self.verifsomme() and self.verifplage()
 
-    def verifierPlagesConsécutives(self):
+    def verifierPlagesConsecutives(self):
         plages_triees = sorted(self.classes.keys(), key=lambda plage: plage[0])
         
         for i in range(1, len(plages_triees)):
@@ -139,7 +129,7 @@ class Tab:
                 return False
             
                 
-        return self.verifierPlagesConsécutives()
+        return self.verifierPlagesConsecutives()
     
     def calculerMoyennePonderee(self) -> float:
         somme_ci_ni = 0
@@ -155,23 +145,6 @@ class Tab:
         else:
             return None
 
-        
-    def estimerPlagesManquantes(self):
-        moyenne_globale = self.calculerMoyennePonderee()
-        
-        for plage, data in list(self.classes.items()):
-            exmin, exmax = plage
-            ni = data[0]
-            
-            if exmin is None and exmax is not None:
-                
-                exmin_estime = 2 * moyenne_globale - exmax
-                self.classes[(exmin_estime, exmax)] = self.classes.pop((exmin, exmax))
-            
-            if exmax is None and exmin is not None:
-                
-                exmax_estime = 2 * moyenne_globale - exmin
-                self.classes[(exmin, exmax_estime)] = self.classes.pop((exmin, exmax))
 
     def variance(self) -> float:
         try:
@@ -183,37 +156,10 @@ class Tab:
                 somme += ni * (ci - moy) ** 2
             return somme / self.total
         except Exception as e:
-            print("Erreur dans les données : "+str(e))
+            print("Erreur dans les donnees : "+str(e))
             self.AfficherFormules()
             return None
 
-    def Findni(self) -> None :
-        try:
-            somme = 0
-
-            for plage in self.classes.keys():
-                somme += self.classes[plage]
-
-            for plage in self.classes.keys():
-                ni = self.classes[plage]
-                if ni == None:
-                    self.classes[plage] = self.total - somme
-                    break
-        except Exception as e:
-            print("Erreur dans les données : "+str(e))
-    
-    def Findplage(self) -> None:
-        try:
-            for plage in self.classes.keys():
-                x,y = plage
-                if x == None:
-                    self.classes[plage] = (self.total - y, y)
-                    break
-                elif y == None:
-                    self.classes[plage] = (x, self.total - x)
-                    break
-        except Exception as e:
-            print("Erreur dans les données : "+str(e))
 
     def moyenne(self) -> float:
         try:
@@ -224,132 +170,116 @@ class Tab:
                 somme += ni * ci
             return somme / self.total
         except Exception as e:
-            print("Erreur dans les données : "+str(e))
+            print("Erreur dans les donnees : "+str(e))
             return None
-
-    def diagramme_differentiel(self):
-        try:
-            valeurs = [data[0] for _, data in self.classes.items()]  
-            plt.hist(valeurs, bins=len(self.classes))
-            plt.show()
-        except Exception as e:
-            print("Erreur dans les données : "+str(e))
-
-    def diagramme_integral(self):
-        try:
-            valeurs = [float(data[0]) for _, data in self.classes.items()]
-            valeurs_cumulees = []
-            somme_cumulee = 0
-            for valeur in valeurs:
-                somme_cumulee += valeur
-                valeurs_cumulees.append(somme_cumulee)
-
-            indices = [i for i in range(len(valeurs_cumulees))]
-
-            plt.plot(valeurs_cumulees)
-            plt.show()
-        except Exception as e:
-            print("Erreur dans les données : "+str(e))
-
-    def diagramme_en_barres(self):
-        print("Diagramme en barres...")
-        # Supposons que `categories` et `valeurs` sont définies comme dans ton code
-        categories = [i for i, _ in enumerate(self.classes.keys())]
-        valeurs = [ni for _, (ni, _, _, _, _, _) in self.classes.items()]
-        
-        # Définition des couleurs
-        couleur_barre = kd.color(255, 0, 0)  # Rouge
-        
-        # Dimensions de l'écran NumWorks
-        ecran_largeur, ecran_hauteur = 320, 240  # Ajuster selon les spécifications exactes
-        
-        # Dessiner chaque barre
-        for i, val in enumerate(valeurs):
-            print("Dessin de la barre", i)
-            # Calculer la position et la hauteur de la barre
-            x = 10 + i * 20  # Exemple de positionnement, à ajuster
-            y = ecran_hauteur - val  # Ajuster la hauteur
-            kd.fill_rect(int(x), int(y), 10, int(val), couleur_barre)
-
 
 
     def calculer_coefficients(self):
         moy = self.calculerMoyennePonderee()
-        s = self.variance() ** 0.5  # Écart-type
+        s = self.variance() ** 0.5  
         n = self.total
         m3 = sum([(data[1] - moy) ** 3 * data[0] for _, data in self.classes.items()]) / n
         m4 = sum([(data[1] - moy) ** 4 * data[0] for _, data in self.classes.items()]) / n
-        skewness = m3 / (s ** 3)  # Coefficient d'asymétrie
-        kurtosis = m4 / (s ** 4) - 3  # Coefficient d'aplatissement (Fisher)
+        skewness = m3 / (s ** 3)  
+        kurtosis = m4 / (s ** 4) - 3  
         return skewness, kurtosis
     
     def quartiles_interpolation(self):
-
-        total = sum([data[0] for data in self.classes.values()])  # Recalculer au cas où
-        q_positions = [total * 0.25, total * 0.5, total * 0.75]
-        quartiles = []
-
+        # Calculer les positions des quartiles
+        Q1_pos = self.total / 4
+        Median_pos = self.total / 2
+        Q3_pos = 3 * self.total / 4
+        
+        # Initialisation des variables pour stocker les resultats des quartiles
+        Q1 = None
+        Median = None
+        Q3 = None
+        
+        # Frequence cumulee precedente
+        F_prev = 0
+        
+        # Trier les classes par leur limite inferieure pour s'assurer de l'ordre
         sorted_classes = sorted(self.classes.items(), key=lambda x: x[0][0])
-        cumul = 0  # Fréquence cumulée
-        for pos in q_positions:
-            for (exmin, exmax), (ni, _, _, ai, Fi, _) in sorted_classes:
-                cumul += ni
-                if cumul >= pos:
-                    prev_cumul = cumul - ni
-                    delta = (pos - prev_cumul) / ni
-                    quartile = exmin + delta * ai
-                    quartiles.append(quartile)
-                    break
+        
+        for (exmin, exmax), data in sorted_classes:
+            ni = data[0]  # Nombre d'appareils dans la classe
+            Fi = F_prev + ni  # Frequence cumulee jusqu'à cette classe
+            ai = exmax - exmin  # Amplitude de la classe
+            
+            # Calcul de Q1 si pas encore trouve et que la position est atteinte
+            if Q1 is None and Fi >= Q1_pos:
+                Q1 = exmin + ((Q1_pos - F_prev) / ni) * ai
+            
+            # Calcul de la Mediane
+            if Median is None and Fi >= Median_pos:
+                Median = exmin + ((Median_pos - F_prev) / ni) * ai
+            
+            # Calcul de Q3
+            if Q3 is None and Fi >= Q3_pos:
+                Q3 = exmin + ((Q3_pos - F_prev) / ni) * ai
+            
+            F_prev = Fi  # Mise à jour de la frequence cumulee pour la prochaine iteration
+            
+            # Arrêter les calculs si tous les quartiles sont trouves
+            if Q1 is not None and Median is not None and Q3 is not None:
+                break
+        
+        return Q1, Median, Q3
+        
+    #! ecart type
+    def ecart_type(self):
+        try:
+            return self.variance() ** 0.5
+        except Exception as e:
+            print("Erreur dans les donnees : "+str(e))
+            return None
 
-        if len(quartiles) == 3:
-            return quartiles
-        else:
-            return [None, None, None]  # Retourne une liste de None si les quartiles ne peuvent pas être calculés
+    #! coefficient de variation
+    def coefficient_variation(self):
+        try:
+            return self.ecart_type() / self.moyenne()
+        except Exception as e:
+            print("Erreur dans les donnees : "+str(e))
+            return None
+        
 
-    
-    def dessiner_box_plot(self):
-        quartiles = self.quartiles_interpolation()
-        if not all(quartiles):
-            print("Erreur lors du calcul des quartiles")
-            return
-
-        q1, mediane, q3 = quartiles
-        sorted_classes = sorted(self.classes.keys(), key=lambda plage: plage[0])
-        min_val, _ = sorted_classes[0]
-        _, max_val = sorted_classes[-1]
-
-        x_start, y_start = 50, 100
-        box_width = 20
-        scale = 2
-
-        # Adaptez cette section en fonction de votre interface graphique
-        # Dessiner la boîte
-        kd.fill_rect(int(x_start + q1*scale), y_start, int((q3-q1)*scale), box_width, kd.color(200, 200, 200))
-        # Dessiner la médiane
-        kd.fill_rect(int(x_start + mediane*scale) - 1, y_start, 3, box_width, kd.color(255, 0, 0))
-        # Dessiner les whiskers (optionnel)
-        # Ajouter du texte pour les labels (optionnel)
     
     def AfficherFormules(self)->None:
         
         print("ci = (exmax + exmin)/2")
-        print("ci = min + max /2, c'est la moyenne des extrémités")
+        print("ci = min + max /2, c'est la moyenne des extremites")
         print("fi = ni/total fi")
-        print("fi = ni/n, c'est la fréquence de la classe")
+        print("fi = ni/n, c'est la frequence de la classe")
         print("ai = exmax - exmin")
         print("ai = max - min, c'est la largeur de la classe")
         print("Fi = somme(fi), simga fi, du genre f1 f2 f3 ... fn")
-        print("Fi = somme(fi), simga fi, c'est la fréquence cumulée de la classe")
+        print("Fi = somme(fi), simga fi, c'est la frequence cumulee de la classe")
         print("hi = (fi/ai)*" + str(self.coef) + "hi")
-        print("hi = (fi/ai)*"+str(self.coef)+"HI, c'est la densité de la classe")
+        print("hi = (fi/ai)*"+str(self.coef)+"HI, c'est la densite de la classe")
         print("Moyenne = somme(ci*ni)/total")
-        print("Moyenne = somme(ci*ni)/total, c'est la moyenne pondérée")
+        print("Moyenne = somme(ci*ni)/total, c'est la moyenne ponderee")
         print("Variance = somme(ni*(ci-moyenne)^2)/total")
-        print("Variance = somme(ni*(ci-moyenne)^2)/total, c'est la variance pondérée")
+        print("Variance = somme(ni*(ci-moyenne)^2)/total, c'est la variance ponderee")
         print("Quartiles = interpolation des quartiles, exmin + (((pos - prev_Fi) / fi) * ai)")
-        print("Calcul des coefficients d'asymétrie et d'aplatissement formule: skewness = m3 / (s ** 3) et kurtosis = m4 / (s ** 4) - 3")
+        print("Calcul des coefficients d'asymetrie et d'aplatissement formule: skewness = m3 / (s ** 3) et kurtosis = m4 / (s ** 4) - 3")
+        print("""
+    Interpolation Lineaire = L + (((N / 2) - F) / f) * C
+
+    * L est la limite inferieure de la classe mediane,
+     
+    * N est le nombre total d'observations,
+    
+    * F est la frequence cumulee avant la classe mediane,
+    
+    * f est la frequence de la classe mediane,
+    
+    * C est l'amplitude de la classe mediane.
+    """)
 
     def toString(self)-> None:
+        
+        # dict_items([((15000.0, 35000.0), [8160.0, 25000.0, 20000.0, 20000.0, 10000.0]), ((85000.0, 103905.60875512997), [3655.0, 94452.80437756499, 18905.60875512997, 38905.60875512997, 4859.353023909987])])
+
         try:
             for plage, (ni, ci, fi, ai, Fi, hi) in self.classes.items():
                 print("Plage: "+str(plage)+"ni: "+str(ni)+"ci: "+str(ci)+"fi: "+str(fi)+"ai: "+str(ai)+"Fi: "+str(Fi)+"hi: "+str(hi))
@@ -377,7 +307,43 @@ class Tab:
         except Exception as e:
             print("Erreur dans les coefficients : "+str(e))
 
+        try:
+            print("Ecart type: "+str(self.ecart_type()))
+        except Exception as e:
+            print("Erreur dans l'ecart type : "+str(e))
+        
+        try:
+            print("Coefficient de variation: "+str(self.coefficient_variation()))
+        except Exception as e:
+            print("Erreur dans le coefficient de variation : "+str(e))
 
+
+#! definition de la classe Tab2 en deduire quels fonction sont utilisable
+    def Type(self):
+        print("Variables qualitatives : Representent des caracteristiques non numeriques, comme le genre, la couleur, etc.")
+        print("Variables quantitatives : Representent des mesures numeriques ou des quantites.")
+    
+    def Paremetre(self):
+        print("Moyenne (ou espérance) : Indique la valeur moyenne d'une distribution de données.")
+        print("Médiane : La valeur médiane sépare l'échantillon en deux parties égales.")
+        print("Mode : La valeur qui apparaît le plus fréquemment dans un ensemble de données.")
+        print("Écart-type : Mesure la dispersion des valeurs autour de la moyenne.")
+        print("Variance : La moyenne des carrés des écarts par rapport à la moyenne.")
+        print("Minimum et maximum : Les valeurs les plus petites et les plus grandes dans un ensemble de données.")
+        print("Quartiles : Les valeurs qui divisent un ensemble de données ordonné en quatre parties égales.")
+        print("Étendue : La différence entre la valeur maximale et la valeur minimale.")
+        print("Skewness (asymétrie) : Mesure l'asymétrie de la distribution des données par rapport à la moyenne.")
+        print("Kurtosis (aplatissement) : Mesure à quel point la distribution des données est concentrée autour de la moyenne.")
+    
+    def Cours(self):
+        print("fi/j = (fij)/(f.j)")
+        print("fj/i = (fji)/(fi.)")
+        print("Lorsque X et Y indépendants")
+        print("fij = (f.i)^fi/j = fi.*fj/i = f.j*fi.")
+        print("nij/n = n.j/n * ni./n <=> nij = ni.*n.j/n")
+        print("Khi-deux = somme((ni,j - n_i.n_.j)^2 / (n_i.n_.j))")
+        print("ni,j: Fréquence observée dans la cellule (i, j).")
+        print("n_i. et n_.j: Totaux marginaux de la ligne i et de la colonne j.")
 def main():
     print("Configuration de la table des classes")
 
@@ -405,22 +371,23 @@ def main():
     for i in range(size):
         while True:
             try:
-                masse = float(input("masse du sujet pour la classe "+str(i+1)+" : "))
+                masse = float(input("masse classe "+str(i+1)+" : "))
                 masse_salariale.append(masse)
                 break
             except ValueError:
                 print("Veuillez entrer un nombre pour la masse salariale.")
-    
 
+    
     tab = Tab(total, size, coef, None, masse_salariale)
     while True:
         i = 1 
         print("\nMenu:")
         print("{}. Remplir classes".format(i)); i += 1
         print("{}. Calculer statistiques".format(i)); i += 1
-        print("{}. Verifier Données".format(i)); i += 1
+        print("{}. Verifier Donnees".format(i)); i += 1
         print("{}. Afficher formules".format(i)); i += 1
-        print("{}. Afficher données".format(i)); i += 1
+        print("{}. Afficher donnees".format(i)); i += 1
+        print("{}. Afficher definition".format(i)); i += 1
         if diagrammes:
             print("{}. Diagrammes".format(i)); i += 1
         print("{}. Quitter".format(i))
@@ -431,20 +398,36 @@ def main():
             tab.remplirClasses()
         elif choix == '2':
             tab.calculer()
-            print("Calculs effectués.")
+            print("Calculs effectues.")
         elif choix == '3':
-            if tab.verif() and tab.verifierPlagesConsécutives():
-                print("Toutes les vérifications sont passées.")
+            if tab.verif() and tab.verifierPlagesConsecutives():
+                print("Toutes les verifications sont passees.")
             else:
-                print("Erreur dans les données.")
+                print("Erreur dans les donnees.")
         elif choix == '4':
             tab.AfficherFormules()
         elif choix == '5':
             tab.toString()
-        elif choix == '6' and diagrammes:
+        elif choix =='6':
+            print("1. Type")
+            print("2. Parametre")
+            print("3. Cours")
+            print("4. Retour au menu principal")
+            definition_choix = input("Entrez votre choix: ")
+            if definition_choix == '1':
+                tab.Type()
+            elif definition_choix == '2':
+                tab.Paremetre()
+            elif definition_choix == '3':
+                tab.Cours()
+            elif definition_choix == '4':
+                continue
+            else:
+                print("Choix invalide, veuillez reessayer.")
+        elif choix == '7' and diagrammes:
             print("Diagrammes:")
-            print("1. Diagramme différentiel")
-            print("2. Diagramme intégral")
+            print("1. Diagramme differentiel")
+            print("2. Diagramme integral")
             print("3. Diagramme en barres")
             print("4. Diagramme circulaire")
             print("5. Box plot")
@@ -457,19 +440,19 @@ def main():
             elif diagramme_choix == '3':
                 tab.diagramme_en_barres()
             elif diagramme_choix == '4':
-                #tab.diagramme_circulaire()
-                print("Non implémenté.")
+                print("Non implemente.")
             elif diagramme_choix == '5':
                 tab.dessiner_box_plot()
             elif diagramme_choix == '6':
                 continue
             else:
-                print("Choix invalide, veuillez réessayer.")
+                print("Choix invalide, veuillez reessayer.")
         elif choix == str(i):
             print("Fin.")
             break
         else:
-            print("Choix invalide, veuillez réessayer.")
+            print("Choix invalide, veuillez reessayer.")
+
             
 
 main()
